f3ee20df452e5e0c83b20ffc0dc1075e1e7b4ab0
Artsiom Patseshkin <artsiom.patseshkin@softclub.by>
2020-06-08 09:38:36 +0300

    Initial commit

---
+++ src/main/java/io/vaultproject/SpringVaultDemoApplication.java
@@ -0,0 +1,13 @@
+package io.vaultproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaultDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVaultDemoApplication.class, args);
+    }
+
+}
